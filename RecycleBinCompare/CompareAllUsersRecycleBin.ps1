85171e26b15bdef279598d0412f7b7c421a2e567
SeekNotSought <crlkernek@gmail.com>
2026-02-15 20:55:58 -0500

    Created a header file for the CompareAllUsersRecycleBin.ps1

---
+++ RecycleBinCompare/CompareAllUsersRecycleBin.ps1
@@ -0,0 +1,25 @@
+#requires -Version 5.1
+<#
+.SYNOPSIS
+    Grabs files in all users's Recycle Bine. Looks at the orginial location to see if the file still exists in that location.
+
+.DESCRIPTION
+    This script checks all users's Recycle Bin on the device. It creates a table of all the files in the Recycle Bin and looks to see if the file still exists in the original location. It outputs the results to a csv.
+
+.PARAMETER IPAddress
+    The file location to output the csv.
+
+.EXAMPLE
+    .\NetworkTroubleshooter.ps1 -OutputFile "C:/Output.csv"
+    Runs the script and outputs the csv to the file location
+
+.NOTES
+    Author: SeekNotSought
+    Version: 0.16
+    Last Updated: 2026-02-14
+
+.REQUIREMENTS
+    PowerShell 5.1 (required for specific versions of the the Test-Connection commands)
+    Windows OS
+#>
+# Purpose: Compare all user recycle bins to see if the files no longer exist in their original location.
